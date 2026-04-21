32b7773636593f5e6f6588563851907a574af559
isaismael04@gmail.com <isaismael04@gmail.com>
2026-04-21 15:00:33 -0300

    Final de consulta

--- main.c
+++ main.c
@@ -20,6 +20,9 @@ void crearTareas(Nodo *tareasPendientes);
 Tarea crearTarea(int id, char *descripcion, int duracion);
 Nodo * crearNodos(Tarea tarea);
 void insertarNodo(Nodo **tareas, Nodo *nuevoNodo);
+void mostrarTareas(Nodo *listaTareas);
+void completarTarea(Nodo *tareasPendientes, Nodo *tareasRealizadas, Tarea tareaCompletada);
+int menuPrincipal();
 
 
 int main(){
@@ -28,7 +31,31 @@ int main(){
     tareasPendientes = crearListaVacia();
     Nodo *tareasRealizadas;
     tareasRealizadas = crearListaVacia();
-    crearTareas(tareasPendientes);
+    int opcionPrincipal = menuPrincipal();
+    while(opcionPrincipal != 0){
+        switch (opcionPrincipal){
+        case 1:
+            crearTareas(tareasPendientes);
+            break;
+        case 2:
+            if(tareasPendientes != NULL){
+                mostrarTareas(tareasPendientes);
+            } else {
+                printf("\nNo se han cargado tareas. No hay nada que mostrar...");
+            }
+            break;
+        case 3:
+            if(tareasRealizadas != NULL){
+                mostrarTareas(tareasRealizadas);
+            } else {
+                printf("\nNo se han completado tareas. No hay nada que mostrar...");
+            }
+            break;
+        default:
+            break;
+        }
+        opcionPrincipal = menuPrincipal();
+    }
     return 0;
 }
 
@@ -52,8 +79,8 @@ void crearTareas(Nodo *tareasPendientes){
             if(tareasPendientes == NULL){
                 id = 1000;
             } else {
-                Nodo *aux = tareasPendientes->Siguiente;
-                id = aux->T.TareaID+1;
+                Nodo *aux = tareasPendientes;
+                id = aux->T.TareaID + 1;
             }
             char descripcion[100];
             getchar();
@@ -86,4 +113,27 @@ Nodo * crearNodos(Tarea tarea){
 void insertarNodo(Nodo **tareas, Nodo *nuevoNodo){
     nuevoNodo->Siguiente = *tareas;
     *tareas = nuevoNodo;
+}
+
+int menuPrincipal(){
+    int opcion;
+    do{
+        printf("\n\n\n---- MENU PRINCIPAL -----\n");
+        printf("0. Salir\n1. Carga de tareas\n2. Consultar tareas pendientes\n3. Consultar tareas realizadas\n4. Buscar tarea\n\nElige una opcion (0-4): ");
+        scanf("%d", &opcion);
+    }while(opcion<0 || opcion>4);
+    return opcion;
+
+}
+
+void mostrarTareas(Nodo *listaTareas){
+    Nodo *aux = listaTareas;
+    while(aux != NULL){
+        printf("\n---------------\nID de la tarea: %d\nDescripcion de la tarea: %s\nDuracion de la tarea: %d", aux->T.TareaID, aux->T.Descripcion, aux->T.Duracion);
+        aux = aux->Siguiente;
+    }
+}
+
+void completarTarea(Nodo *tareasPendientes, Nodo *tareasRealizadas, Tarea tareaCompletada){
+
 }
